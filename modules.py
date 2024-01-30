f74040dd8a0531c7233aea5ca1e925bf76078698
Panapv02 <pablopervaz.com>
2024-01-30 19:08:55 +0100

    función weather

--- modules.py
+++ modules.py
@@ -1,5 +1,12 @@
 import requests;
 
 def get_weather():
-    print('klk')
-    pass;
+    url = 'https://servizos.meteogalicia.gal/mgrss/predicion/jsonPredConcellos.action';
+    response = requests.get(url, params={'idConc':'27049'})
+    tMin = response.json()['predConcello']['listaPredDiaConcello'][0]['tMin'];
+    tMax = response.json()['predConcello']['listaPredDiaConcello'][0]['tMax'];
+    choiva = response.json()['predConcello']['listaPredDiaConcello'][0]['pchoiva'];
+    maña = choiva['manha'];
+    tarde = choiva['tarde'];
+    noite = choiva['noite']
+    return f'Temperaturas:\nMínima: {tMin}\tMáxima: {tMax}\nProbabilidades de choiva:\nMañán: {maña}%\tTarde: {tarde}%\tNoite: {noite}%';
